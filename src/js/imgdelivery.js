4578485f8bd3c2cecacfa69f3703c80476ee327e
nezu <mel@nezu.world>
2024-12-19 14:36:01 -0500

    removed uploadcare config from config.yml (first steps towards migrating off uploadcare)

--- src/js/imgdelivery.js
+++ src/js/imgdelivery.js
@@ -1,14 +1,16 @@
-(function(src, cb) {
-  var s = document.createElement("script"); s.setAttribute("src", src);
-  s.onload = cb; (document.head || document.body).appendChild(s);
-})("https://ucarecdn.com/libs/blinkloader/3.x/blinkloader.min.js", function() {
+(function (src, cb) {
+  var s = document.createElement("script");
+  s.setAttribute("src", src);
+  s.onload = cb;
+  (document.head || document.body).appendChild(s);
+})("https://ucarecdn.com/libs/blinkloader/3.x/blinkloader.min.js", function () {
   window.Blinkloader.optimize({
-    pubkey:"68f7ba8bc14a6aeec189",
-    fadeIn:true,
-    lazyload:true,
-    smartCompression:true,
-    responsive:true,
-    retina:true,
-    webp:true
+    pubkey: "ecf994d8839fd7737aa3", // using lisa's uploadcare key for testing
+    fadeIn: true,
+    lazyload: true,
+    smartCompression: true,
+    responsive: true,
+    retina: true,
+    webp: true,
   });
 });
